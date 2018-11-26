bd01a04a9f95cd9b0cea9a6ec5fc8d555130a36a
Jakub Zięba <beewasp1997@gmail.com>
2018-11-26 18:59:47 +0100

    kolokwium

---
+++ navbar.js
@@ -0,0 +1,14 @@
+let nav= '<nav><div class="navbar">\n' +
+    '    \n' +
+    '<ul class="nav-menu">\n' +
+    '    <li><a href="index.html">Blog</li>\n' +
+    '    <li><a href="index.html">Index</li>\n' +
+    '</ul>\n' +
+    '\n' +
+    '</div>' +
+    '</nav>'
+
+
+let navbar=document.querySelector('.navbar');
+
+navbar.innerHTML = nav;
